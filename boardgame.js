b927f02f2dbe39c96222f1aa8898fed06cdc09e3
catmiller07 <catmiller07@comcast.net>
2016-01-20 21:58:16 -0500

    second commit

--- boardgame.js
+++ boardgame.js
@@ -16,7 +16,7 @@ function displayDiceRoll(iDice,iDieFace,iRollVal){
     //console.log("here i am");
 	
    iDice.src = "Dodecahedron.gif";
-   faceValue = dieRoll();
+   //faceValue = dieRoll();
 	
    setTimeout(function(){
 	    iDice.src = "dodecahedron-frame1.png";
@@ -28,20 +28,44 @@ function displayDiceRoll(iDice,iDieFace,iRollVal){
 }
 
 var faceValue = 0;
+
 var virtDie0 = document.getElementById("anim-die0");
 var dieFace0 = document.getElementById("roll-val0");
-
 // for start
+
+var position1 = 0;
 virtDie0.onclick=function(){
     faceValue = dieRoll();	
 	displayDiceRoll(virtDie0,dieFace0,faceValue); 
+	//console.log(faceValue);
 	
-	var r = 0;
-	for(var p = 1; p < faceValue; p++){
-		var cell = "#r" + r +"c" + p;
-		$(cell).html(".play-mov");
-	};
+	var base = 60;
+	
+    position1 = position1 + faceValue;
+	if ( position1 <= 11 ) {
 		
+		$("#player1").css("transform", "translateX(" + (position1 * base) + "px)" );
+	}
+	else if (position1 <= 22 ) {
+			$("#player1").css("transform", "translate(660px, " + ((position1- 11 ) * base) + "px)" );
+	}
+	else if (position1 <= 33){
+		var transformStr = "translate(" +  ( -(position1-33) )* base + "px, 660px)";
+		console.log("position1: "+position1);
+		console.log(transformStr);
+		$("#player1").css("transform", transformStr );
+	}
+	else{
+		//var transformStr = "translate(0px," +  Math.ceil(660-position1-43) + "px)";
+		var newpos = Math.abs(660-( Math.abs(position1-44) * base) );
+		var transformStr = "translate(0px," +  newpos + "px)";
+		console.log(transformStr);
+		$("#player1").css("transform", transformStr );
+	} 	
+		
+	
+	
+	
 };
 
 
